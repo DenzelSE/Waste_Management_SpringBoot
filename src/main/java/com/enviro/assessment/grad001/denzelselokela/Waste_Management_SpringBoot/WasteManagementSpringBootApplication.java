93ec6f387f86fc413e41eb16b35e6bfd492dfaca
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-01-19 00:13:59 +0200

    Project intiated and basic layers

---
+++ src/main/java/com/enviro/assessment/grad001/denzelselokela/Waste_Management_SpringBoot/WasteManagementSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.denzelselokela.Waste_Management_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteManagementSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteManagementSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/denzelselokela/Waste_Management_SpringBoot/WasteManagementSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.denzelselokela.Waste_Management_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteManagementSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
