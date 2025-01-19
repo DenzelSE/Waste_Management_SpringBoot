1ab1838c7db6e71cfa3ba47d6cb04b9f4095ae36
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-01-20 00:58:34 +0200

    simple data for database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO waste_categories (id, name, description) VALUES
+(1, 'Organic', 'Biodegradable waste from plants or animals'),
+(2, 'Plastic', 'Synthetic or semi-synthetic materials'),
+(3, 'Paper', 'Materials made from wood pulp or other fibrous substances');
+
+
+INSERT INTO disposal_guidelines (id, name, description) VALUES
+(1, 'Organic Waste Disposal', 'Compost if possible, or dispose in green bins'),
+(2, 'Plastic Waste Disposal', 'Clean and segregate by type, then place in recycling bins'),
+(3, 'Paper Waste Disposal', 'Separate from other waste and place in paper recycling bins');
---
+++ src/test/java/com/enviro/assessment/grad001/denzelselokela/Waste_Management_SpringBoot/ControllerTests/WasteGuidelineControllerTest.java
@@ -0,0 +1,8 @@
+package com.enviro.assessment.grad001.denzelselokela.Waste_Management_SpringBoot.ControllerTests;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WasteGuidelineControllerTest {
+    
+}
